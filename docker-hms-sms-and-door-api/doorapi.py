0c4cef027f2990c0b095da9c548e4655e1d36c55
Recolic <git@me.recolic.net>
2024-04-05 20:23:12 -0700

    quick_push

---
+++ docker-hms-sms-and-door-api/doorapi.py
@@ -0,0 +1,85 @@
+import time
+import pytz
+from datetime import datetime, timedelta
+from smartrent import async_login
+import asyncio
+import os
+
+HTTP_AUTH_TOKEN = 'R_SEC_HMSAPI_KEY'
+
+def get_api():
+    async def _wrapped():
+        return await async_login('bensong.liu@microsoft.com', 'R_SEC_SMARTRENT_KEY')
+    return asyncio.run(_wrapped())
+
+alarm_muted_until = datetime.now(pytz.utc)
+
+def mute_alarm_once():
+    alarm_muted_until = now + timedelta(minutes=30)
+    mute_log_message = f"{pst_now.strftime('%Y-%m-%d %H:%M:%S')} PST - Alarm muted for 30 minutes"
+    print(mute_log_message)
+
+def beep_until_muted():
+    while now > alarm_muted_until:
+        os.system("echo -e '\a' > /dev/tty0")
+        time.sleep(0.15)
+
+def check_lock_status_and_beep():
+    global lock, alarm_muted_until
+
+    now = datetime.now(pytz.utc)
+    pst_now = now.astimezone(pytz.timezone("US/Pacific"))
+
+    if pst_now.hour >= 0 and pst_now.hour < 8:
+        locked = get_api().get_locks()[0].get_locked()
+        print(f"{pst_now.strftime('%Y-%m-%d %H:%M:%S')} PST - Door lock status: {'locked' if locked else 'unlocked'}")
+        if not locked:
+            beep_until_muted()
+
+# ====================== start http server =====================
+from threading import Thread
+import http.server, socketserver
+import subprocess
+
+class my_handler(http.server.BaseHTTPRequestHandler):
+    def do_HEAD(self):
+        self.send_header("Content-type", "text/plain; charset=utf-8")
+        self.end_headers()
+        self.send_response(200)
+    def do_GET(self):
+        stat = [400, 'bad query']
+        if HTTP_AUTH_TOKEN not in self.path:
+            stat = [403, 'invalid auth token']
+        else:
+            if self.path.startswith('/mute'):
+                stat = [200, 'ok']
+                mute_alarm_once()
+                self.wfile.write('ok'.encode('utf-8'))
+                return
+            elif self.path.startswith('/unlock'):
+                # TODO unlock
+                stat = [200, 'ok']
+            elif self.path.startswith('/lock'):
+                # TODO lock
+                stat = [200, 'ok']
+
+        self.send_response(stat[0])
+        self.send_header("Content-type", "text/plain; charset=utf-8")
+        self.end_headers()
+        self.wfile.write(stat[1].encode('utf-8'))
+
+def http_thread(arg):
+    listen_port = 30802
+    server = http.server.HTTPServer(('', listen_port), my_handler)
+    print('Listening *:' + str(listen_port))
+    server.serve_forever()
+
+thread = Thread(target = http_thread, args = (0, ))
+thread.start()
+print("started http thread")
+# ====================== end http server =====================
+
+while True:
+    check_lock_status_and_beep()
+    time.sleep(4)
+
