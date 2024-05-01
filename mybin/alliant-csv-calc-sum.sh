e6e6551ef608155f6c013daeaa8edf886eacd4cf
Recolic <git@me.recolic.net>
2024-05-01 00:05:57 -0700

    quick_push

--- mybin/alliant-csv-calc-sum.sh
+++ mybin/alliant-csv-calc-sum.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 [ "$1" = "" ] && echo "Usage: $0 <alliant.csv>" && exit 1
-expr_=`cat "$1" | sed 's/\$0.00/NUL/g' | # strip 0.00 fees
+cat "$1" | sed 's/\$0.00/NUL/g' | # strip 0.00 fees
     sed -E 's/\(\$([0-9]*\.[0-9][0-9])\)/$-\1/g' | # replace ($1.11) to $-1.11
     grep -o '\$[0-9-]*\.[0-9][0-9]' | # grep all price tags
     tr -d '$' | # remove leading dollar sign
     tr '\n' '+' | # join strings
-    sed 's/+$//' # remove tail plus sign
-`
+    sed 's/+$//' > /tmp/.altmp # remove tail plus sign
+
+expr_=`cat /tmp/.altmp`
 res=`python -c "print('%.2f' %  ($expr_)  )"`
 
 echo "$expr_ = $res"
