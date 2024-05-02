801f1f104efea28c3c4c6d2606b5b29d87509ade
Recolic <git@me.recolic.net>
2024-05-01 17:09:46 -0700

    quick_push

--- mybin/alliant-csv-calc.sh
+++ mybin/alliant-csv-calc.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-[ "$1" = "" ] && echo "Usage: $0 <alliant.csv>" && exit 1
+[ ! -f "$1" ] && echo "Usage: $0 <alliant.csv>" && exit 1
+
+## calc sum
+
 cat "$1" | sed 's/\$0.00/NUL/g' | # strip 0.00 fees
     sed -E 's/\(\$([0-9]*\.[0-9][0-9])\)/$-\1/g' | # replace ($1.11) to $-1.11
     grep -o '\$[0-9-]*\.[0-9][0-9]' | # grep all price tags
@@ -9,5 +12,11 @@ cat "$1" | sed 's/\$0.00/NUL/g' | # strip 0.00 fees
 
 expr_=`cat /tmp/.altmp`
 res=`python -c "print('%.2f' %  ($expr_)  )"`
-
 echo "$expr_ = $res"
+
+## calc 2.5 discount
+
+expr_="$res * 0.975"
+res=`python -c "print('%.2f' %  ($expr_)  )"`
+echo "$expr_ = $res"
+
