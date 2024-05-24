314b6eb1e6a1ce3cc16afcd025bbfdd72b770323
Recolic <git@me.recolic.net>
2024-05-24 16:00:33 -0700

    quick_push

--- mymsbin/azvm-deploy.sh
+++ mymsbin/azvm-deploy.sh
@@ -7,19 +7,17 @@ tipid="$3"
 cluster="$4"
 
 vmsize="${vmsize:=Standard_D2_v5}"
+vnet_enc="${vnet_enc:1}"
+vnet_dual_stack="${vnet_dual_stack:1}"
 accelnet=1
 
-prefix="${prefix:=$(head -c8 /dev/urandom | base64 -w0 | tr -d =)}"
+prefix="${prefix:=$(head -c8 /dev/urandom | base64 -w0 | tr -d =/)}"
 resgrp="${resgrp:=rsh-$prefix-resgrp}"
 avname="${avname:=$prefix-av}"
 vnetname="${vnetname:=$prefix-vnet}"
 vmname="${vmname:=$prefix-vm}"
 
-vm_create_xtra_arg=()
-
-[ "$accelnet" = 1 ] && vm_create_xtra_arg+=(--accelerated-networking true)
-[ "$R_SEC_WEAK12" = "" ] && R_SEC_WEAK12=dummypassW12
-
+## Check usage... Script user: Do not modify below this line.
 [ "$vmcount" = "" ] && echo "This script deploys n VMs into the same vnet, and optionally, you can make it deploy into TiP session.
 Usage: $0 <location> <vmcount> [tipid] [cluster]
 Example: $0 eastus2euap 2
@@ -27,7 +25,16 @@ Example: $0 eastus2euap 2
 Optionally, you can override other variables by setting corresponding env.
 " && exit 1
 
+vm_create_xtra_arg=()
+vnet_create_xtra_arg=()
+
+[ "$vnet_enc"        = 1 ] && vnet_create_xtra_arg+=(--enable-encryption true --encryption-enforcement-policy allowUnencrypted)
+[ "$vnet_dual_stack" = 1 ] && addr_prefixes=('10.0.0.0/16' 'fd00:db8:deca::/48') || addr_prefixes=('10.0.0.0/16')
+[ "$accelnet"        = 1 ] && vm_create_xtra_arg+=(--accelerated-networking true)
+[ "$R_SEC_WEAK12"    = "" ] && R_SEC_WEAK12=dummypassW12
+
 ## all args ready to go!
+
 
 echo "Deploying $vmcount $vmsize VMs at location $location, using res_grp $resgrp, vmname $vmname ..."
 az group create -n "$resgrp" --location "$location" > /dev/null 2>&1
@@ -42,6 +49,16 @@ if [ "$tipid" != "" ]; then
     az deployment group create -g "$resgrp" --template-file /tmp/template-avset.json --parameters "avname=$avname" "location=$location" "tipid=$tipid" "cluster=$cluster" || exit $?
     vm_create_xtra_arg+=(--availability-set "$avname")
 fi
+
+# Create vnet with optional dual_stack and encryption support
+echo "Deploying vnet ${vnetname} at location $location, using res_grp $resgrp, vnet_enc $vnet_enc, vnet_dual_stack $vnet_dual_stack ..."
+az network vnet create \
+    --resource-group "${resgrp}" \
+    --location "${location}" \
+    --name "${vnetname}" \
+    --subnet-name default \
+    --address-prefixes "${addr_prefixes[@]}" \
+    ${vnet_create_xtra_arg[@]}
 
 for cter in $(seq $vmcount); do
     echo "EXEC #" az vm create --name "$vmname-$cter" -g "$resgrp" --image Ubuntu2204 --admin-password $R_SEC_WEAK12 --admin-username r --location "$location" --size "$vmsize" --vnet-name "$vnetname" --subnet default "${vm_create_xtra_arg[@]}"
