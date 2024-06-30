e877bb89d274d9bb2234f3196ced02af520f5ee2
Recolic <git@me.recolic.net>
2024-06-29 21:16:40 -0700

    quick_push

--- mybin/jic-backup.sh
+++ mybin/jic-backup.sh
@@ -2,5 +2,6 @@
 # sync current dir to storage.recolic as just-in-case backup
 
 dirname=$(basename $(pwd))
+echo "Uploading to storage.recolic:$dirname"
 rsync -avz --progress . storage.recolic:~/jic-backups/"$dirname"
 
