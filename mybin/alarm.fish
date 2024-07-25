3d10c584f9c62e0e920ea5c8f9c46d8b24eb4780
Recolic <git@me.recolic.net>
2024-07-24 23:23:25 -0700

    .u

--- mybin/alarm.fish
+++ mybin/alarm.fish
@@ -24,16 +24,24 @@ echo Please set MAX volume and MIN light
 pactl set-sink-volume 0 110%
 # pactl set-sink-volume 0 150%
 
+function should_trigger
+    if test (date +%s) -gt $alarm_time
+        return 0
+    end
+    if test (cat /sys/class/power_supply/AC/online) != 1
+        echo "!!!!!!!!!!!! WARNING !!!!!!!!!!!!!!!! NO AC POWER, system could suspend!"
+        return 0 # ALARM! no power supply.
+    end
+    return 1
+end
+
 set prefix (dirname (status --current-filename))
 
 while true
-    if test (date +%s) -gt $alarm_time # 9:30AM
-        while true
-            pactl set-sink-volume 0 110%
-            env XDG_RUNTIME_DIR=/run/user/(id -u) mpg123 $prefix/alarm.mp3
-            sleep 1
-        end
+    if should_trigger
+        pactl set-sink-volume 0 110%
+        env XDG_RUNTIME_DIR=/run/user/(id -u) mpg123 $prefix/alarm.mp3 > /dev/null 2>&1
     end
-    sleep 5
+    sleep 2
 end
 
