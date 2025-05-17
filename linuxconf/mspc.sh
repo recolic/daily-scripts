a8945e820507059b87292e71036af72c7b55d8ef
Recolic <git@me.recolic.net>
2025-05-16 17:05:43 -0700

    .

--- linuxconf/mspc.sh
+++ linuxconf/mspc.sh
@@ -11,11 +11,10 @@ lc_startup () {
     mkdir -p /extradisk/swtpm/mytpm
     lc_bgrun /tmp/swtpm.log bash -c 'cd /extradisk/swtpm ; while true; do swtpm socket --tpm2 --tpmstate dir=./mytpm --ctrl type=unixio,path=./mytpm.sock; done'
 
-    lc_bgrun /tmp/frpc-cdn.log auto_restart frpc -c files/secrets/mspc-frpc-cdn.ini
-    lc_bgrun /tmp/frpc.log     auto_restart frpc -c files/secrets/mspc-frpc.ini
-    lc_bgrun /tmp/frps.log     auto_restart frps -c files/secrets/mspc-frps.ini
+    lc_bgrun /tmp/frpc.log  auto_restart frpc -c files/secrets/mspc-frpc.ini
+    lc_bgrun /tmp/frps.log  auto_restart frps -c files/secrets/mspc-frps.ini
 
-    lc_bgrun /tmp/cron.log     every 30m bash files/srv-deps/mspc-check-internet.sh
-    lc_bgrun /tmp/cron.log     every 1m  bash files/mspc-simple-vmm/cron-callback.sh
-    lc_bgrun /tmp/cron.log     every 5m  curl https://recolic.net/api/mspc-keepalive.php
+    lc_bgrun /tmp/cron.log  every 30m bash files/srv-deps/mspc-check-internet.sh
+    lc_bgrun /tmp/cron.log  every 1m  bash files/mspc-simple-vmm/cron-callback.sh
+    lc_bgrun /tmp/cron.log  every 5m  curl https://recolic.net/api/mspc-keepalive.php
 }
