24011f9de400b3cdcff8ede92460e8363a70fa9d
Recolic <git@me.recolic.net>
2025-05-21 15:40:59 -0700

    .

--- linuxconf/arch-desktop/40-work.sh
+++ linuxconf/arch-desktop/40-work.sh
@@ -6,11 +6,21 @@ lc_startup () {
         [[ -f /usr/bin/dkms ]] && sed -i 's/sign_file=[^ ]*$/sign_file=fuckyouidiot /g' /usr/bin/dkms
         
         # Microsoft Edge fix
-        if [[ -f /usr/share/applications/microsoft-edge.desktop ]]; then
+        if [ -f /usr/share/applications/microsoft-edge.desktop ]; then
           if ! grep disable-features=msUndersideButton /usr/share/applications/microsoft-edge.desktop; then
             sed -i 's/.usr.bin.microsoft-edge-stable/& --disable-features=msUndersideButton/' /usr/share/applications/microsoft-edge.desktop
           fi  
         fi  
+
+        # Azure VPN fix
+        if [ -f /usr/share/applications/microsoft-azurevpnclient.desktop ]; then
+            sed -i 's|Exec=/opt/microsoft|Exec=env BR=microsoft-edge-stable /opt/microsoft|' /usr/share/applications/microsoft-azurevpnclient.desktop
+        fi
+
+        # azcli fix
+        if [ -f /usr/bin/az ]; then
+            sed -i 's|^/opt/azure-cli|BR=microsoft-edge-stable /opt/azure-cli|' /usr/bin/az
+        fi
     else
         lc_bgrun /dev/null every 1h systemctl restart --user microsoft-identity-broker.service
         # lc_bgrun /dev/null every 30m /etc/ar2/ar2.sh
@@ -25,7 +35,21 @@ lc_startup () {
     fi
 }
 
+
 lc_assert_user_is_not root
+
+lc_init () {
+    echo "[Desktop Entry]
+Version=1.0
+Name=_browser_wrapper
+Exec=bash -c '[[ -z \$BR ]] && BR=firefox ; \$BR \"\$@\"' _ %U
+StartupNotify=true
+Terminal=false
+Type=Application
+Categories=Network;WebBrowser;
+MimeType=text/html;text/xml;application/xhtml_xml;image/webp;x-scheme-handler/http;x-scheme-handler/https;x-scheme-handler/ftp;" | sudo tee /usr/share/applications/default-browser.desktop
+    xdg-settings set default-web-browser default-browser.desktop
+}
 
 lc_login () {
     if ! grep -F .m.recolic /etc/hosts > /dev/null; then
