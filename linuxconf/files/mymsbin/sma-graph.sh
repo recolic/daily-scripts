8a1c4e1fa736238d89607fd44dda619eac5de1c3
Recolic <git@me.recolic.net>
2025-06-03 18:45:29 -0700

    .

--- linuxconf/files/mymsbin/sma-graph.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-"$HOME/ms-scripts/$(basename $0)" "$@"
--- quick_push.sh
+++ quick_push.sh
@@ -14,13 +14,3 @@ git add -A
 git commit -m ".$1" || true # fail if no change
 git push
 
-# copy to mirror
-msmirror=$HOME/code/msdoc/proj/sh-mirror
-if [[ -d $msmirror ]]; then
-    rm -rf $msmirror/*/ # remove all directories
-    cp -r linuxconf/files/mybin linuxconf/files/mymsbin $msmirror/
-
-    rm -f $msmirror/mymsbin/oespolicy
-    gpg -d -o $msmirror/mymsbin/oespolicy $msmirror/mymsbin/oespolicy.gpg
-fi
-
