e0669c963f79b22951f0bd002adc8afcad60fafe
Recolic <git@me.recolic.net>
2025-06-16 12:35:22 -0700

    .

--- linuxconf/utils/auto-nfs-mgr.sh
+++ linuxconf/utils/auto-nfs-mgr.sh
@@ -6,38 +6,39 @@ if [[ $EUID -ne 0 ]]; then
     exit 1
 fi
 
-HMS_GOOD=0
-HMS_BAD=1
+GOOD=0
+BAD=1
 # Function to check if hms.r is reachable
 check_reachability_i() {
     if ping -c 1 hms.r &> /dev/null; then
-        return $HMS_GOOD # Host is reachable
+        return $GOOD # Host is reachable
     else
-        return $HMS_BAD # Host is unreachable
+        return $BAD # Host is unreachable
     fi
 }
 check_reachability() {
+    ip a | grep "inet 10.100.100" || return $BAD
     for i in {1..20}; do
-        check_reachability_i && return $? ; sleep 0.2
+        check_reachability_i && return $GOOD ; sleep 0.2
     done
-    return $HMS_BAD
+    return $BAD
 }
 check_mounted() {
     # Warning: df -T might hang forever if NFS is offline!
-    df_res=$(df -T) || return $HMS_GOOD # nfs io error. Return mounted=1
-    echo "$df_res" | grep nfs && return $HMS_GOOD || return $HMS_BAD
+    df_res=$(df -T) || return $GOOD # nfs io error. Return mounted=1
+    echo "$df_res" | grep nfs && return $GOOD || return $BAD
 }
 
 while true; do
     check_reachability
     net_stat=$?
-    if [ $net_stat = $HMS_BAD ]; then
+    if [ $net_stat = $BAD ]; then
         # Bug fix: if network unavailable, check_mounted might hang.
         umount -f -l /home/recolic/nfs
     else
         check_mounted
         nfs_stat=$?
-        if [ $nfs_stat = $HMS_BAD ]; then
+        if [ $nfs_stat = $BAD ]; then
             mount -o bg,intr,soft,timeo=1,retrans=1,actimeo=1,retry=1 hms.r:/ /home/recolic/nfs
         fi
     fi
