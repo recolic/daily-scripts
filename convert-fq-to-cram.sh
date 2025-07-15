452ba2ae3e6a442fe186ffe631f10bdc3774daa7
Recolic <git@me.recolic.net>
2025-07-15 11:44:15 -0700

    .

---
+++ convert-fq-to-cram.sh
@@ -0,0 +1,7 @@
+# convert fastq to cram+crai
+minimap2 -t $(nproc) -a -x sr fq-base/Homo_sapiens.GRCh38.dna.primary_assembly.fa NG1CFEHD1B_1.fq NG1CFEHD1B_2.fq  -o CE.sam
+
+samtools view -bS CE.sam | samtools sort -o CE.sorted.bam
+samtools view -C -T fq-base/Homo_sapiens.GRCh38.dna.primary_assembly.fa -o CE.cram CE.sorted.bam
+samtools index CE.cram
+
