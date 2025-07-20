881466458e67d8b42aaf7ce750d2f79241ca636d
Recolic <git@me.recolic.net>
2025-07-19 21:59:32 -0700

    .

--- linuxconf/files/fish-config/functions/rnfs.mount.fish
+++ linuxconf/files/fish-config/functions/rnfs.mount.fish
@@ -1,4 +1,4 @@
 function rnfs.mount
-    ping -c 1 hms.r
-    and sudo mount -o bg,intr,hard,timeo=1,retrans=1,actimeo=1,retry=1 hms.r:/ /home/recolic/nfs
+    ping -c 1 hms.recolic
+    and sudo mount -o bg,intr,hard,timeo=1,retrans=1,actimeo=1,retry=1 hms.recolic:/ /home/recolic/nfs
 end
