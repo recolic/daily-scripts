b03fa62c762a6a4f65c5baeb85c92e02e85c29a8
Recolic <git@me.recolic.net>
2025-08-15 20:19:24 -0700

    .

--- show_gpg_file_receipt.sh
+++ show_gpg_file_receipt.sh
@@ -7,4 +7,10 @@ echo -n "$fname "
 gpg --pinentry-mode cancel --list-packets "$fname" 2>&1 | grep keyid | sed 's/^.*keyid //g'
 exit $?
 
+## usage
+# for fl in (find . -type f)
+#     if file $fl | grep 'PGP message' > /dev/null
+#         ~/sh/show_gpg_file_receipt.sh "$fl"
+#     end
+# end
 
