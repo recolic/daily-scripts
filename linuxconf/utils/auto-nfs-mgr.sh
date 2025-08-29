e236b5592bbd6f985a5b7467b2a6803aee4237a2
Recolic <git@me.recolic.net>
2025-08-29 00:31:12 -0700

    .

--- linuxconf/utils/auto-nfs-mgr.sh
+++ linuxconf/utils/auto-nfs-mgr.sh
@@ -12,6 +12,7 @@ impl_mount () {
     if [ "$IMPL" = nfs ]; then
         mount -o bg,intr,hard,timeo=1,retrans=1,actimeo=1,retry=1 hms.recolic:/ /home/recolic/nfs
     elif [ "$IMPL" = sshfs ]; then
+        echo user_allow_other | sudo tee /etc/fuse.conf
         sshfs hms.recolic:/mnt/fsdisk/nfs /home/recolic/nfs -o reconnect -o allow_other
     fi
 }
