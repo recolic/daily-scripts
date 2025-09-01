6047441cf931eeb744a4a39861f91b520b0c5aa7
Recolic <git@me.recolic.net>
2025-09-01 14:41:01 -0700

    .

--- linuxconf/files/fish-config/functions/rnfs.mount.fish
+++ linuxconf/files/fish-config/functions/rnfs.mount.fish
@@ -2,5 +2,5 @@ function rnfs.mount
     # ping -c 1 hms.recolic
     # and sudo mount -o bg,intr,hard,timeo=1,retrans=1,actimeo=1,retry=1 hms.recolic:/ /home/recolic/nfs
     echo user_allow_other | sudo tee /etc/fuse.conf
-    sshfs hms.recolic:/mnt/fsdisk/nfs /home/recolic/nfs -o reconnect -o allow_other
+    sshfs hms.recolic:/mnt/fsdisk/nfs /home/recolic/nfs -o reconnect -o allow_other -o idmap=user
 end
--- linuxconf/utils/auto-nfs-mgr.sh
+++ linuxconf/utils/auto-nfs-mgr.sh
@@ -13,7 +13,7 @@ impl_mount () {
         mount -o bg,intr,hard,timeo=1,retrans=1,actimeo=1,retry=1 hms.recolic:/ /home/recolic/nfs
     elif [ "$IMPL" = sshfs ]; then
         echo user_allow_other | sudo tee /etc/fuse.conf
-        sshfs hms.recolic:/mnt/fsdisk/nfs /home/recolic/nfs -o reconnect -o allow_other
+        sshfs hms.recolic:/mnt/fsdisk/nfs /home/recolic/nfs -o reconnect -o allow_other -o idmap=user
     fi
 }
 impl_umount () {
