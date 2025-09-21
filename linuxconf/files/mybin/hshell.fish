b82cdf7069115e4daa584736838dfdfebeabc0ba
Recolic <git@me.recolic.net>
2025-09-21 02:57:16 -0700

    .

--- linuxconf/files/mybin/hshell.fish
+++ linuxconf/files/mybin/hshell.fish
@@ -1,6 +1,5 @@
 #!/bin/fish
 
 gpg -d (dirname (status --current-filename))/lib/hshell.env.gpg > /tmp/.hs.fish ; or exit 1
-gpgconf --reload gpg-agent # Next PIN would be required.
 env RECOLIC_ENV_NAME=HSHELL fish --private -C 'source /tmp/.hs.fish'
 
