680114fa52815ef7c1d0f5a4764595dfc0543ac5
Recolic <git@me.recolic.net>
2025-10-07 17:55:31 -0700

    .

--- linuxconf/files/fish-config/functions/mslab-tunnel.fish
+++
@@ -1,9 +0,0 @@
-function mslab-tunnel
-    if test (count $argv) != 0 ; and test $argv[1] = of
-        echo "old fashioned tunnel..."
-        ssh -L 127.0.0.1:10803:127.0.0.1:30002 -Nn ms.recolic
-    else
-        echo "tunnel..."
-        sshpass -p (rsec MSPASS) ssh -D 10809 -Nn (rsec MSID)@jb3.backup2.m.recolic
-    end
-end
