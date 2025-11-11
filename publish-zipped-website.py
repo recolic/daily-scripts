32fee832402f06dd7abaf02630e8fa8d55205351
Recolic <git@me.recolic.net>
2025-11-10 17:57:56 -0800

    .

--- publish-zipped-website.py
+++ publish-zipped-website.py
@@ -1,15 +1,17 @@
 # GPT-5, tested by recolic
 # Used to publish offline MSD handbook: https://github.com/SunsetMkt/MSD-Manual-Portable
+# If you don't have python: unzip and open index.html with browser.
 import http.server
 import socketserver
-import zipfile
+import zipfile, sys
 from urllib.parse import unquote
 
-ZIP_PATH = "site.zip"
+ZIP_PATH = sys.argv[1]
 INDEX_FILE = "index.html"
 PORT = 8001
 
 zip_file = zipfile.ZipFile(ZIP_PATH, 'r')
+socketserver.TCPServer.allow_reuse_address = True
 
 class ZipHandler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
@@ -17,6 +19,7 @@ class ZipHandler(http.server.SimpleHTTPRequestHandler):
         path = unquote(self.path).split('?', 1)[0].lstrip('/')
         if not path:
             path = INDEX_FILE
+        print(path)
         try:
             with zip_file.open(path) as f:
                 data = f.read()
@@ -32,7 +35,7 @@ class ZipHandler(http.server.SimpleHTTPRequestHandler):
         self.wfile.write(data)
 
 with socketserver.TCPServer(("", PORT), ZipHandler) as httpd:
-    print(f"Serving {ZIP_PATH} on port {PORT}...")
+    print(f"Serving {ZIP_PATH} as http://localhost:{PORT}")
     httpd.serve_forever()
 
 
