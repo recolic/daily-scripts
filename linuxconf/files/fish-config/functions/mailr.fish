0cc6b6f9b1a15e561a46faba293355a2204fa036
Recolic <git@me.recolic.net>
2026-01-06 15:57:32 -0800

    aur-clion

--- linuxconf/files/fish-config/functions/mailr.fish
+++ linuxconf/files/fish-config/functions/mailr.fish
@@ -1,10 +1,17 @@
 # Defined interactively
 function mailr
-    # Send email to root@recolic.net.
+    # Send email to root@recolic.net, or otheres
     set -l message $argv[1]
+    set -l recv "root@recolic.net"
     set -l title "[mailr] RECOLIC SHELL NOTIFY"
+    set -l extra ""
 
-    echo ">>> Sending email:" root@recolic.net "$title: $message" 1>&2
-    curl "https://recolic.net/api/email-notify.php?recvaddr=root@recolic.net&b64Title="(echo $title | base64)'&b64Content='(echo "[mailr] $message" | base64 -w 0) 1>&2
+    if test -n "$argv[2]"
+        set recv $argv[2]
+        set extra "&apiKey="(rsec MAILAPI_KEY)
+    end
+
+    echo ">>> Sending email:" $recv "$title: $message" 1>&2
+    curl "https://recolic.net/api/email-notify.php?recvaddr=$recv&b64Title="(echo $title | base64)'&b64Content='(echo "[mailr] $message" | base64 -w 0)$extra 1>&2
     return $status
 end
