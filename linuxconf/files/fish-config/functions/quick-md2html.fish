23eb36dd1eaad8c162e4f5ed8367c212e9b53998
Recolic <git@me.recolic.net>
2026-01-15 17:43:42 -0800

    .

--- linuxconf/files/fish-config/functions/quick-md2html.fish
+++ linuxconf/files/fish-config/functions/quick-md2html.fish
@@ -1,5 +1,5 @@
 function quick-md2html
-    set head '<html lang=en><meta charset=utf-8><meta content="width=device-width,initial-scale=1"name=viewport><link href=https://recolic.cc/res/github-markdown.css rel=stylesheet><style>.markdown-body{box-sizing:border-box;min-width:200px;max-width:980px;margin:0 auto;padding:45px}@media (max-width:767px){.markdown-body{padding:15px}}</style><article class=markdown-body>'
+    set head '<html lang=en><meta charset=utf-8><meta content="width=device-width,initial-scale=1"name=viewport><link href=https://recolic.net/res/github-markdown.css rel=stylesheet><style>.markdown-body{box-sizing:border-box;min-width:200px;max-width:980px;margin:0 auto;padding:45px}@media (max-width:767px){.markdown-body{padding:15px}}</style><article class=markdown-body>'
     set tail '</article></html>'
     echo $head
     echo '<!-- markdown.css can also fetch from https://raw.githubusercontent.com/sindresorhus/github-markdown-css/main/github-markdown.css -->'
--- linuxconf/hms/zfs-monitor-failure-daemon.fish
+++ linuxconf/hms/zfs-monitor-failure-daemon.fish
@@ -11,7 +11,7 @@ while true
 
         set email_content (echo "Detected zpool fail: $output" | base64 -w 0)
         set email_title (echo "[hms.recolic] ALERT: Zpool failure" | base64 -w 0)
-        curl "https://recolic.cc/api/email-notify.php?recvaddr=root@recolic.net&b64Title=$email_title&b64Content=$email_content" 1>&2
+        curl "https://recolic.net/api/email-notify.php?recvaddr=root@recolic.net&b64Title=$email_title&b64Content=$email_content" 1>&2
     end
     if not zpool status | grep 'errors: No known data errors'
         set output (zpool status)
@@ -19,7 +19,7 @@ while true
 
         set email_content (echo "Detected zpool fail: $output" | base64 -w 0)
         set email_title (echo "[hms.recolic] ALERT: Zpool failure" | base64 -w 0)
-        curl "https://recolic.cc/api/email-notify.php?recvaddr=root@recolic.net&b64Title=$email_title&b64Content=$email_content" 1>&2
+        curl "https://recolic.net/api/email-notify.php?recvaddr=root@recolic.net&b64Title=$email_title&b64Content=$email_content" 1>&2
     end
     sleep 1h
 end
