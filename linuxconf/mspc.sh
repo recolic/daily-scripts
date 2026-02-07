03c15cb65a1fd571e3acd2f367bfa91be8f67524
Recolic <git@me.recolic.net>
2026-02-06 18:09:36 -0800

    .

--- linuxconf/mspc.sh
+++ linuxconf/mspc.sh
@@ -12,7 +12,7 @@ lc_startup () {
     lc_bgrun /tmp/swtpm.log bash -c 'cd /extradisk/swtpm ; while true; do swtpm socket --tpm2 --tpmstate dir=./mytpm --ctrl type=unixio,path=./mytpm.sock; done'
 
     # For emergency access only
-    lc_bgrun /tmp/frpc.log  auto_restart bash -c "curl -s https://recolic.net/api/ms-rpctl.php | grep rp.enabled=1 && frpc tcp -n mspc_ssh -l 22 -r 30622 -s proxy-cdn.recolic.net -P 30999 --token $(rsec FRP_KEY) ; sleep 10m"
+    lc_bgrun /tmp/frpc.log  auto_restart bash -c "curl -s https://recolic.net/api/ms-rpctl.php | grep rp.enabled=1 && frpc tcp -n mspc_ssh -l 22 -r 30622 -s proxy-cdn.recolic.net:30999 --token $(rsec FRP_KEY) ; sleep 10m"
     # Note: 30474 -> 30623
 
     lc_bgrun /tmp/cron.log  every 30m bash utils/mspc-check-internet.sh
