79c3633d9369c2b57873e01feae3dd94eb9dd259
Recolic <git@me.recolic.net>
2026-02-18 06:00:29 -0800

    .

--- linuxconf/files/fish-config/functions/msvpn-oneclick.fish
+++ linuxconf/files/fish-config/functions/msvpn-oneclick.fish
@@ -1,6 +1,11 @@
 function msvpn-oneclick
     while true
-        sudo -E gpclient --fix-openssl connect --browser microsoft-edge-stable --gateway Redmond-WA --disable-ipv6 https://msftvpn-alt.ras.microsoft.com
-        sleep 1
+        sudo -E gpclient --fix-openssl connect --browser microsoft-edge-stable --gateway Beijing-CN --disable-ipv6 https://msftvpn-alt.ras.microsoft.com
+
+        # tmp: global VPN
+        sudo ip r add default dev tun0  metric 98
+        echo -e "nameserver 10.50.10.50\nnameserver 1.1.1.1" | sudo tee /etc/resolv.conf
+
+        sleep 1 ; or break
     end
 end
