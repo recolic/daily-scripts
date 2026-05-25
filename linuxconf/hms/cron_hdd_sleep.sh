a0704cd1c2466f71559634c0f9aa36910eaae976
Recolic <git@me.recolic.net>
2026-05-25 00:05:56 -0700

    .

--- linuxconf/hms/cron_hdd_sleep.sh
+++ linuxconf/hms/cron_hdd_sleep.sh
@@ -2,28 +2,15 @@
 # call me every 10min
 set -euo pipefail
 
-STATE_FILE="/tmp/.sleeping"
-
 ensure_sleep() {
-  if [[ -f "$STATE_FILE" ]] && [[ "$(cat "$STATE_FILE")" == "1" ]]; then
-    return
-  fi
-
   systemctl stop nginx
   sleep 5
   hdparm -y /dev/disk/by-id/ata-ST4000NM0035-1V4107_ZC14G486
   hdparm -y /dev/disk/by-id/ata-TOSHIBA_HDWE140_Z9JZK1XJFBRG
-
-  echo 1 > "$STATE_FILE"
 }
 
 ensure_active() {
-  if [[ -f "$STATE_FILE" ]] && [[ "$(cat "$STATE_FILE")" == "0" ]]; then
-    return
-  fi
-
-  systemctl start nginx
-  echo 0 > "$STATE_FILE"
+    systemctl is-active --quiet nginx || systemctl start nginx
 }
 
 hour=$(TZ=America/Los_Angeles date +%H)
