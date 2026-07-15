a877e08ef9ecf45f1c82f71a12472786a85b29a8
Recolic <git@me.recolic.net>
2026-07-14 18:24:29 -0700

    .

---
+++ mcp-flight-search.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+### rsandbox bash
+# git clone https://github.com/ravinahp/flights-mcp
+# cd flights-mcp
+# curl -LsSf https://astral.sh/uv/install.sh | sh
+#manual# add "httpcore>=1.0.9" into pyproject.toml
+# uv sync
+
+source "$HOME/.local/bin/env"
+export DUFFEL_API_KEY_LIVE="$1"
+exec "$HOME/.local/bin/uv" --directory /root/flights-mcp run flights-mcp
+
+#manual# vscode add mcp server: 
+######## bash -c exec rsandbox /root/mcp-wrapper.sh $(rsec DUFFEL_API)
