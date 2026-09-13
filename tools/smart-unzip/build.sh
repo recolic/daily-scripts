04ad533a648a93c6b1857c240a68dac8e4a83abd
Recolic <git@me.recolic.net>
2026-09-13 13:27:29 -0700

    .

--- tools/smart-unzip/build.sh
+++ tools/smart-unzip/build.sh
@@ -6,4 +6,4 @@ cd -- "$(dirname -- "$0")"
 export CGO_ENABLED=0 GOARCH=amd64
 GOOS=linux go build -trimpath -ldflags='-s -w' -o smart-unzip-linux-amd64.out .
 GOOS=windows go build -trimpath -ldflags='-s -w' -o smart-unzip-windows-amd64.exe .
-printf '%s\n' 'Built smart-unzip-linux-amd64 and smart-unzip-windows-amd64.exe'
+printf '%s\n' 'Built smart-unzip-linux-amd64.out and smart-unzip-windows-amd64.exe'
