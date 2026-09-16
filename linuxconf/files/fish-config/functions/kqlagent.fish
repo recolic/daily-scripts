a05ac8595c34107aa08b18659579f98aff07c105
Recolic <git@me.recolic.net>
2026-09-16 15:30:19 -0700

    .

--- linuxconf/files/fish-config/functions/kqlagent.fish
+++ linuxconf/files/fish-config/functions/kqlagent.fish
@@ -1,5 +1,5 @@
 function kqlagent --wraps='cd ~/code/aitools/plugins/AdaptiveKusto/ ; and  agency copilot' --description 'alias kqlagent=cd ~/code/aitools/plugins/AdaptiveKusto/ ; and  agency copilot'
   cd ~/code/aitools/plugins/AdaptiveKusto/
-  and agency copilot --allow-all-urls $argv --no-auto-update
+  and p 2 agency copilot --allow-all-urls $argv --no-auto-update
         
 end
